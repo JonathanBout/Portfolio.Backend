23e26ed7da792b6516208c568f83b5df290956a7
Jonathan Bout <jonathanbout01@gmail.com>
2024-12-27 13:47:29 +0100

    docstrings and cache key prefix in CacheServiceOutput class

--- Portfolio.Backend/Services/Caching/ICacheServiceOutput.cs
+++ Portfolio.Backend/Services/Caching/ICacheServiceOutput.cs
@@ -6,18 +6,41 @@ namespace Portfolio.Backend.Services.Caching
 {
 	public interface ICacheServiceOutput<TModel, TResult>
 	{
+		/// <summary>
+		/// Get the result from the cache. If the result is not in the cache, it will be fetched and stored in the cache.
+		/// </summary>
+		/// <param name="model">The arguments to use when fetching</param>
+		/// <param name="forceFetch">Wheter a fetch should be forced</param>
 		Task<TResult?> Get(TModel model, bool forceFetch = false);
 	}
 
-	public abstract class CacheServiceOutput<TModel, TResult>(IMemoryCache cache) : ICacheServiceOutput<TModel, TResult>
+	public abstract class CacheServiceOutput<TModel, TResult> : ICacheServiceOutput<TModel, TResult>
 	{
-		protected IMemoryCache Cache => cache;
+		/// <summary>
+		/// The prefix to use for the cache key. This is the name of the derived class.
+		/// </summary>
+		private readonly string CacheKeyPrefix;
 
+		/// <summary>
+		/// The cache to use for storing the data.
+		/// </summary>
+		protected IMemoryCache Cache { get; }
+
+		/// <summary>
+		/// The expiration time for a cache entry.
+		/// </summary>
 		abstract protected TimeSpan EntryExpiration { get; }
+
+
+		protected CacheServiceOutput(IMemoryCache cache)
+		{
+			Cache = cache;
+			CacheKeyPrefix = GetType().Name + "_";
+		}
 
 		public Task<TResult?> Get(TModel model, bool forceFetch = false)
 		{
-			string cacheKey = GetCacheKey(model);
+			string cacheKey = CacheKeyPrefix + GetCacheKey(model);
 			if (forceFetch)
 			{
 				Cache.Remove(cacheKey);
